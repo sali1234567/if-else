f9f4f2933d07ee6f01fc64bfe76f9776a425b1b2
sali1234567 <sali.chuguashvili@gmail.com>
2023-08-01 20:51:53 +0400

    first commit

---
+++ ifElse.js
@@ -0,0 +1,9 @@
+let Name = "sali";
+
+if(Name == "sali"){
+    console.log("This is my nickname")
+}else if(Name == "salome"){
+    console.log("This is my name")
+}else{
+    console.log("This isn't my name/nickname")
+}
